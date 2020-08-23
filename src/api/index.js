54972678f7800249f83aa05cb8d3d5597637e661
ahmedali8 <imahmedalibhatti@gmail.com>
2020-08-23 21:11:28 +0500

    added a functionality when user changes the country it reflects on the cards - we changed the url in api - we are managing the state in App.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,20 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-// fetching tha data through the api 
-export const fetchData = async () => {
-    try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+// fetching tha data through the api, also for a particular country
+export const fetchData = async (country) => {
+    let changeableUrl = url;
 
-        return { confirmed, recovered, deaths, lastUpdate };
+    if(country) {
+        changeableUrl = `${url}/countries/${country}`;
+    }
+
+
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+
+        return { confirmed, recovered, deaths, lastUpdate }; 
+
     } catch (error) {
         console.log('fetchDataError: ', error);
     }
