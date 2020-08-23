bab6cb3f66fa7a8e3c81ad443983ce04e9a4ce36
ahmedali8 <imahmedalibhatti@gmail.com>
2020-08-23 09:02:03 +0500

    completed the Cards component. - changed the font to 'Montserrat' - made it responsive - used countup for animation - used cx for multiple classNames - used date object to convert api fetched date to human readable format

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,11 @@ const url = 'https://covid19.mathdro.id/api';
 
 // fetching tha data through the api 
 export const fetchData = async () => {
-    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
-    return { confirmed, recovered, deaths, lastUpdate };
+        return { confirmed, recovered, deaths, lastUpdate };
+    } catch (error) {
+        console.log('fetchData: ', error);
+    }
 };
